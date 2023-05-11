4cc385c4252594cdb99088c1c28f29f04702648d
Gabriel Alpha Aguilar <gabrielalpha98@gmail.com>
2023-05-11 09:19:56 +0800

    First Commit

--- ClinicalSystem/index.aspx.cs
+++ ClinicalSystem/index.aspx.cs
@@ -23,43 +23,41 @@ namespace ClinicalSystem
 
                 SqlDataReader reader = sqlCommand.ExecuteReader();
 
-                if(reader.HasRows){
-                    while (reader.Read()) {
-                        // if user is found.
-                        if (reader["EMPUSERNAME"].ToString() == Login_Username_TextBox.Text || reader["EMPPASSWORD"].ToString() == Login_Password_TextBox.Text)
-                        {
-                            if (reader["EMPPASSWORD"].ToString() == Login_Password_TextBox.Text) // check if password is correct
-                            {
-                                // For Doctors and nurses 
-                                if (reader["EMPPOSITION"].ToString().ToUpper().Equals("DOCTOR") || reader["EMPPOSITION"].ToString().ToUpper().Equals("NURSE"))
-                                {
-                                    Session["User_First_Name"] = reader["EMPFNAME"].ToString();
-                                    Session["User_Last_Name"] = reader["EMPLNAME"].ToString();
-                                    Session["User_Position"] = reader["EMPPOSITION"].ToString();
-                                    Response.Redirect("prescription.aspx", false);
-                                    Context.ApplicationInstance.CompleteRequest();
-                                }
+                if (!reader.HasRows) return;
 
-                                // For Secretaries
-                                else { 
-                                    Session["User_First_Name"] = reader["EMPFNAME"].ToString();
-                                    Session["User_Last_Name"] = reader["EMPLNAME"].ToString();
-                                    Session["User_Position"] = reader["EMPPOSITION"].ToString();
-                                    Response.Redirect("billing.aspx", false);
-                                    Context.ApplicationInstance.CompleteRequest();
-                                }
-                            }
-                            else { 
-                                NoticeLabel.Text = "Incorrect password.";
-                            }
-                        }
-                        else
-                        {
-                            NoticeLabel.Text = "User is not found!";
-                        }
+                while (reader.Read())
+                {
+                    // if user is found.
+                    if (reader["EMPUSERNAME"].ToString() != Login_Username_TextBox.Text || reader["EMPPASSWORD"].ToString() != Login_Password_TextBox.Text) {
+                        NoticeLabel.Text = "User is not found!";
+                        return;
                     }
-                    sqlConnection.Close();  
+
+                    // check if password is correct
+                    if (reader["EMPPASSWORD"].ToString() != Login_Password_TextBox.Text) {
+                        NoticeLabel.Text = "Incorrect password.";
+                        return;
+                    }
+
+                    // For Doctors and nurses 
+                    if (reader["EMPPOSITION"].ToString().ToUpper().Equals("DOCTOR") || reader["EMPPOSITION"].ToString().ToUpper().Equals("NURSE"))
+                    {
+                        Session["User_First_Name"] = reader["EMPFNAME"].ToString();
+                        Session["User_Last_Name"] = reader["EMPLNAME"].ToString();
+                        Session["User_Position"] = reader["EMPPOSITION"].ToString();
+                        Response.Redirect("prescription.aspx", false);
+                        Context.ApplicationInstance.CompleteRequest();
+                    }
+                    else // For Secretaries
+                    {
+                        Session["User_First_Name"] = reader["EMPFNAME"].ToString();
+                        Session["User_Last_Name"] = reader["EMPLNAME"].ToString();
+                        Session["User_Position"] = reader["EMPPOSITION"].ToString();
+                        Response.Redirect("billing.aspx", false);
+                        Context.ApplicationInstance.CompleteRequest();
+                    }
                 }
+                sqlConnection.Close();
             }catch(Exception ex)
             {
                 Response.Write(ex.Message);
